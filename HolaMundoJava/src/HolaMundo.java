ba945592eeaa079b45ed544f1c2c53de2895c66d
gcardosov <ger9047@gmail.com>
2020-06-26 01:25:35 -0500

    Variables en Java

--- HolaMundoJava/src/HolaMundo.java
+++ HolaMundoJava/src/HolaMundo.java
@@ -1,9 +1,11 @@
-//Comentario simple
-//Mi clase en java
+///////////////////////////////////////
+//Comentario simple                 ///
+//Mi clase en java                  ///
+//Autor Gerardo Cardoso             ///
+///////////////////////////////////////
 
 public class HolaMundo {
     public static void main (String args[]){
         System.out.println("Hola Mundo desde java");
-    
     }    
 }
---
+++ HolaMundoJava/src/Variables.java
@@ -0,0 +1,14 @@
+
+public class Variables {
+    public static void main (String args[]){
+        //Reglas
+            //Tipo
+            //Identificador
+            //Inicializacion
+            //Notacion de camello
+            
+            int miVariableEntera = 10;
+            System.out.println(miVariableEntera);
+    
+    }
+}
