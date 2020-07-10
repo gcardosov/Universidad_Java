d80bad8da83b2e131fe97bbaa367870aff6e20d4
Gerardo Cardoso Vera <35967332+gcardosov@users.noreply.github.com>
2020-07-09 19:44:32 -0500

    Variables tipo String

--- HolaMundoJava/src/Variables.java
+++ HolaMundoJava/src/Variables.java
@@ -7,8 +7,17 @@ public class Variables {
             //Inicializacion
             //Notacion de camello
             
+            //Definimos la variable 
             int miVariableEntera = 10;
             System.out.println(miVariableEntera);
-    
+            //Modificamos el valor de la variable 
+            miVariableEntera = 5;
+            System.out.println(miVariableEntera);
+               //Variable tipo String 
+            String miVariableCadena = "Saludos";
+            System.out.println(miVariableCadena);
+            miVariableCadena = "Adios";
+            System.out.println(miVariableCadena);
+            
     }
 }
