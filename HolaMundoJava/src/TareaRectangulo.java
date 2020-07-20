3f2d29394009433413903426289187a6c646af95
Gerardo Cardoso Vera <35967332+gcardosov@users.noreply.github.com>
2020-07-20 01:27:12 -0500

    Ejecicio rectangulo

---
+++ HolaMundoJava/src/TareaRectangulo.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+
+public class TareaRectangulo {
+    
+       public static void main(String args[]){
+           
+           System.out.println("Programa que calcula el area y perimetro de un rectangulo");
+           System.out.println("Calculemos el perimetro");
+           System.out.println("Proporciona el alto: ");
+           Scanner consola = new Scanner(System.in);
+           int alto = Integer.parseInt(consola.nextLine());
+           System.out.println("Proporciona el ancho: ");
+           int ancho = Integer.parseInt(consola.nextLine());
+           var perimetro = (alto + ancho) * 2;
+           var area = alto * ancho; 
+           System.out.println("Area: " + area);           
+           System.out.println("Perimetro: " + perimetro);
+                            
+       }    
+}
