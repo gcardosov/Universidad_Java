7290d8feb429a9cd4782fbfe0d80f614dc4cae77
Gerardo Cardoso Vera <35967332+gcardosov@users.noreply.github.com>
2020-08-21 15:06:40 -0500

    Uso de las palabras return y null

--- HolaMundoJava/src/PasoPorReferencia/PasoPorReferencia.java
+++ HolaMundoJava/src/PasoPorReferencia/PasoPorReferencia.java
@@ -11,17 +11,25 @@ public class PasoPorReferencia {
         //Aqui como si se altera el valor guardado en memoria
         //mas no se manda un copia
         //si podemos alterar el valor desde otro metodo
-        Persona persona1 = new Persona();
-        persona1.nombre = "Julieta";
-        System.out.println("persona1 nombre: " + persona1.nombre);  
-        cambiarValor(persona1);
-        System.out.println("persona1 cambio de nombre = " + persona1.nombre);
+        //Persona persona1 = new Persona();
+        Persona persona1  = null;
+        //persona1.nombre = "Julieta";
+        //System.out.println("persona1 nombre: " + persona1.nombre);  
+        persona1 = cambiarValor(persona1);
+        //System.out.println("persona1 cambio de nombre: " + persona1.nombre);
     }
     
-    public static void cambiarValor(Persona persona){
-        persona.nombre = "Martin";
+    public static Persona cambiarValor(Persona persona){
+        if(persona == null){
+            System.out.println("Valor de persona invalido : null");
+            return null;
         
+        }
+        //Si tenemos el void en el metodo
+        //y vemos un return; es normal 
+        //despues de la palabra return ya no podemos agregar nada
+        persona.nombre = "Julieta";
+        return persona;
     }
-    
-    
+        
 }
