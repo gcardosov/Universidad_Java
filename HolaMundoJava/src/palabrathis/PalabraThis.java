105f57804ec2e7769df2ab82616781c80a6f4548
Gerardo Cardoso Vera <35967332+gcardosov@users.noreply.github.com>
2020-08-22 10:58:12 -0500

    Uso de la palabra this

--- HolaMundoJava/src/palabrathis/PalabraThis.java
+++ HolaMundoJava/src/palabrathis/PalabraThis.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package palabrathis;
 
-/**
- *
- * @author gcardoso
- */
 public class PalabraThis {
-    
+    public static void main(String[] args) {
+        Persona persona = new Persona("Juan", "Perez");
+        System.out.println("persona = " + persona);
+        System.out.println("persona nombre: " + persona.nombre);
+        System.out.println("persona apellido: " + persona.apellido);
+        
+    }    
 }
+
+class Persona{
+    String nombre;
+    String apellido;
+        
+    Persona(String nombre, String apellido){
+        //super(); llamada al constructor de la clase padre (object)
+        this.nombre = nombre;
+        this.apellido = apellido;
+        //Como podemos mandar a imprimir el objeto 
+        //persona dentro de la misma clase
+        System.out.println("Objeto persona usando this " + this );
+        //tambien se pueden crear objeto con la siguiente sintexis 
+        new Imprimir().imprimir(this);       
+        
+    }
+}
+
+    class Imprimir{
+                
+        public void imprimir(Persona persona){
+            System.out.println("persona desde imprimir: " + persona);
+            System.out.println("impresion del objeto actual(this) " + this);
+    
+        }    
+    }
+    
