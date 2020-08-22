e05cd4329f4a4a3f5d76f171bfa03835f25da4cd
Gerardo Cardoso Vera <35967332+gcardosov@users.noreply.github.com>
2020-08-22 12:37:57 -0500

    Encapsulamiento

---
+++ Encapsulamiento/src/dominio/Persona.java
@@ -0,0 +1,57 @@
+
+package dominio;
+
+
+public class Persona {
+    //Vamos a empezar a manipular los modificadores de 
+    //acceso de los atributos para ver el concepto de 
+    //encapsulamient empzamos con private
+    
+    //ATRIBUTOS
+    private String nombre;
+    private double sueldo;
+    private boolean eliminado;
+    
+    
+    //CONTRUCTOR CON PARAMETROS
+    public Persona(String nombre, double sueldo, boolean eliminado){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.eliminado = eliminado;
+                
+    }
+    
+    //METODOS GETTERS Y SETTERS
+    //GET recupera información
+    //SET modifica información
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public double getSueldo(){
+        return this.sueldo;
+    }
+    
+    public void setSueldo(double sueldo){
+        this.sueldo = sueldo;
+    }
+    
+    //Para los tipos boolean se ocupa "is"    
+    public boolean isEliminado(){
+        return this.eliminado;
+    }
+    
+    public void setEliminado(boolean eliminado){
+        this.eliminado = eliminado;
+    }
+    
+    
+    
+    
+    
+}
---
+++ Encapsulamiento/src/test/PersonaPrueba.java
@@ -0,0 +1,35 @@
+
+package test;
+import dominio.Persona;
+
+public class PersonaPrueba {    
+    public static void main(String[] args) {
+        
+        Persona persona1 = new Persona("Gerardo", 11000.00, false);
+            /*System.out.println("persona1 nombre: " + persona1.getNombre());
+
+            //anteriormente cambiavamos los valores directamente
+            //persona1.nombre = "Leonardo"
+            //pero como usamos el modificador de acceso a private  
+            //por eso usamos set
+            persona1.setNombre("Leonardo");
+            //lo mismo pasa para imprimir  
+            //System.out.println("nombre: " + persona1.nombre);
+            //por eso usamos get
+            System.out.println("persona1 nombre con cambio: " + persona1.getNombre());
+            System.out.println("persona sueldo: " + persona1.getSueldo());
+            System.out.println("persona eliminado: " + persona1.isEliminado());
+            */
+            
+        Persona persona2 = new Persona("Julieta", 15000.00, true);
+            System.out.println("persona2 nombre: " + persona2.getNombre());
+            persona2.setNombre("Blanca");
+            System.out.println("persona2 nombre con cambio:" + persona2.getNombre());
+            System.out.println("persona sueldo: " + persona2.getSueldo());
+            System.out.println("persona eliminada" + persona2.isEliminado());
+            
+            
+    }
+    
+    
+}
