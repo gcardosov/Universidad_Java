4b7b147917d0a16715181fde62b4115b9ffe881b
Gerardo Cardoso Vera <ger9047@gmail.com>
2020-10-11 12:40:36 -0500

    Herencia en Java

---
+++ Herencia/src/test/testHerencia.java
@@ -0,0 +1,18 @@
+
+package test;
+
+import domain.Cliente;
+import domain.Empleado;
+import java.util.Date;
+
+public class testHerencia {
+    public static void main(String[] args) {
+        //Empleado empleado1 = new Empleado("Gerardo", 11000);
+        //System.out.println("empleado1 = " + empleado1);
+        
+        var fecha = new Date();
+        Cliente cliente1 = new Cliente (fecha, true, "Julieta", 'F', 59,"Calle falsa 123" );
+        System.out.println("cliente1 = " + cliente1);
+        
+    }    
+}
